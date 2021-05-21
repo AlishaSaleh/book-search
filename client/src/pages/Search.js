f828e9b4795822e0601da2a8d58b554703c2961c
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-05-21 10:13:57 +0100

    Hero/styling/font

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Jumbotron from '../components/Jumbotron';
 
 function Search() {
     return (
         <>
+        <Jumbotron />
         <h1>Home/Search page</h1>
         </>
     )
